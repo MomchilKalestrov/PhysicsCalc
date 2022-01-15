42f529a337d7374724cf6030ba1c6ff14200d000
Momchil-k <kalestrov.mo@gmail.com>
2022-01-15 10:49:39 +0200

    Initial commit

---
+++ Calc/UserControl2.Designer.cs
@@ -0,0 +1,398 @@
+﻿
+namespace CalcGUI
+{
+    partial class UserControl2
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button25 = new System.Windows.Forms.Button();
+            this.button27 = new System.Windows.Forms.Button();
+            this.button28 = new System.Windows.Forms.Button();
+            this.button29 = new System.Windows.Forms.Button();
+            this.button30 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 23);
+            this.button1.TabIndex = 61;
+            this.button1.Text = "Отправно тяло";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(0, 67);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(233, 23);
+            this.button23.TabIndex = 83;
+            this.button23.Text = "Моментна скорост";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(0, 38);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(233, 23);
+            this.button24.TabIndex = 84;
+            this.button24.Text = "Механично движение";
+            this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 23);
+            this.button2.TabIndex = 85;
+            this.button2.Text = "Ускорение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(233, 23);
+            this.button3.TabIndex = 86;
+            this.button3.Text = "Равноускорително движение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(233, 23);
+            this.button4.TabIndex = 87;
+            this.button4.Text = "Свободно падане";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 183);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(233, 23);
+            this.button5.TabIndex = 88;
+            this.button5.Text = "Равнозакъснително движение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 212);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(233, 23);
+            this.button6.TabIndex = 89;
+            this.button6.Text = "Спирачен път";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(0, 241);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(233, 23);
+            this.button8.TabIndex = 91;
+            this.button8.Text = "Инертност";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(0, 270);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(233, 23);
+            this.button9.TabIndex = 92;
+            this.button9.Text = "Маса";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(0, 299);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(233, 23);
+            this.button10.TabIndex = 93;
+            this.button10.Text = "Първи принцип на механиката";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(0, 328);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(233, 23);
+            this.button11.TabIndex = 94;
+            this.button11.Text = "Втори принцип на механиката";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(239, 9);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(236, 23);
+            this.button12.TabIndex = 95;
+            this.button12.Text = "Трети принцип на механиката";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(239, 38);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(236, 23);
+            this.button13.TabIndex = 96;
+            this.button13.Text = "Сили";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(239, 67);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(236, 23);
+            this.button14.TabIndex = 97;
+            this.button14.Text = "Механична Работа";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(239, 96);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(236, 23);
+            this.button15.TabIndex = 98;
+            this.button15.Text = "Сили - посока на движението";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(239, 125);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(236, 23);
+            this.button16.TabIndex = 99;
+            this.button16.Text = "Мощност";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(239, 183);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(236, 23);
+            this.button18.TabIndex = 101;
+            this.button18.Text = "Кинетична енергия";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(239, 270);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(236, 23);
+            this.button19.TabIndex = 102;
+            this.button19.Text = "Изменение на потенциалната енергия";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(239, 241);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(236, 23);
+            this.button20.TabIndex = 103;
+            this.button20.Text = "Механична енергия";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(239, 299);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(236, 23);
+            this.button21.TabIndex = 104;
+            this.button21.Text = "Закон за запазване на механична енергия";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(239, 212);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(236, 23);
+            this.button22.TabIndex = 105;
+            this.button22.Text = "Потенциална енергия";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button25
+            // 
+            this.button25.Location = new System.Drawing.Point(239, 154);
+            this.button25.Name = "button25";
+            this.button25.Size = new System.Drawing.Size(236, 23);
+            this.button25.TabIndex = 106;
+            this.button25.Text = "Енергия в природата";
+            this.button25.UseVisualStyleBackColor = true;
+            this.button25.Click += new System.EventHandler(this.button25_Click);
+            // 
+            // button27
+            // 
+            this.button27.Location = new System.Drawing.Point(481, 167);
+            this.button27.Name = "button27";
+            this.button27.Size = new System.Drawing.Size(237, 23);
+            this.button27.TabIndex = 108;
+            this.button27.Text = "Хидростатично налягане";
+            this.button27.UseVisualStyleBackColor = true;
+            this.button27.Click += new System.EventHandler(this.button27_Click);
+            // 
+            // button28
+            // 
+            this.button28.Location = new System.Drawing.Point(481, 138);
+            this.button28.Name = "button28";
+            this.button28.Size = new System.Drawing.Size(237, 23);
+            this.button28.TabIndex = 109;
+            this.button28.Text = "Хидралични машини";
+            this.button28.UseVisualStyleBackColor = true;
+            this.button28.Click += new System.EventHandler(this.button28_Click);
+            // 
+            // button29
+            // 
+            this.button29.Location = new System.Drawing.Point(239, 328);
+            this.button29.Name = "button29";
+            this.button29.Size = new System.Drawing.Size(237, 23);
+            this.button29.TabIndex = 110;
+            this.button29.Text = "Налягане";
+            this.button29.UseVisualStyleBackColor = true;
+            this.button29.Click += new System.EventHandler(this.button29_Click);
+            // 
+            // button30
+            // 
+            this.button30.Location = new System.Drawing.Point(481, 196);
+            this.button30.Name = "button30";
+            this.button30.Size = new System.Drawing.Size(237, 23);
+            this.button30.TabIndex = 111;
+            this.button30.Text = "Зависимост между (Ро) и (h)";
+            this.button30.UseVisualStyleBackColor = true;
+            this.button30.Click += new System.EventHandler(this.button30_Click);
+            // 
+            // UserControl2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button30);
+            this.Controls.Add(this.button29);
+            this.Controls.Add(this.button28);
+            this.Controls.Add(this.button27);
+            this.Controls.Add(this.button25);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button1);
+            this.Name = "UserControl2";
+            this.Size = new System.Drawing.Size(718, 364);
+            this.Load += new System.EventHandler(this.UserControl2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button25;
+        private System.Windows.Forms.Button button27;
+        private System.Windows.Forms.Button button28;
+        private System.Windows.Forms.Button button29;
+        private System.Windows.Forms.Button button30;
+    }
+}
