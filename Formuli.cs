4568088bf43f77d86c696acbeb7e3b229654388d
Momchil-k <84711602+Momchil-k@users.noreply.github.com>
2022-02-15 20:55:59 +0200

    Add files via upload

---
+++ Formuli.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace materialUI
+{
+    public partial class Formuli : UserControl
+    {
+        public Formuli()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Тяло, спрямо което определяме положението и движението на другите тела.");
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Промяната на местоположението на едно тяло спрямо отправно тяло.");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Бързината, с която се движи тяло в дадент момент от време.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Физичната величина, която характеризира бързината на движещото тяло.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Движение, при което се увеличава скоростта с постоянно ускорение.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Падане на различни тела във вакуум с еднакво ускорение, което не зависи от тяхната форма, маса, обем и плътност.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Движение, при което скоростта намалява с постоянно ускорение.");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разстоянието от момента на натискане на спирачките до момента на спиране.");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Error 204: \n No input.");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Свойството на телата да запазват състоянието си на покой или на праволинейно равномерно движение.");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Количествената мярка за инертността на телата.");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Всяко тяло запазва състоянието си на покой или на праволинейно равномерно движение, докато външно въздействие не го изведе от това състояние.");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Равнодействащата сила F e равна на произведението на масата m и ускоринието a.");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Всяко действие има равно по сила и противоположно по посока противодействие.");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Физична величина, която определя действието на едно тяло върху друго тяло.");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Една сила може да извършва работа само когато тялото се премества.");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A = Работа; F = Сила; S = Път; A = F*s > 0; A = -F*s < 0; A = 0.");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мощността P е равна на отношението на работата A и времето t за нейното извършване.");
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Енергията в природата може да се превръща от един вид в дръг и да се предава от едно тяло на дръго тяло, но не може да се създава или да изчезва.");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Кинетичната енергия характеризира състоянието на движение на тялото и е равна на половината от произведението на масата и квадрата на скоростта му.");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Изменението на потенциалната енергия е равно на работата на силата на тежестта със знак минус.");
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Сумата от кинетичната и потенциалната енергия на тяло наричаме механична енергия.");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Механичната енергия на тяло, на което действа само силата на тежестта, е постоянна величина, която не се изменя с времето E=const.");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Потенциалната енегия на тяло, на което действа силата на тежестта, е равна на произведенито от неговата маса m, земното ускорение g и височината h, на която то се намира.");
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Налягане, създадено от външна сила върху течност или газ, се предава без изменение във всяка точка на течността или газа.");
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("F2/F1 = S2/S1");
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Налягането на течността, създадено от нейното тегло, се нарича хидростатично налягане.");
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Error 204: \n No input.");
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хидростатичното налягане (Ро) на дадена дълбочина h в течността зависи от дълбочината h и от нейната плътност.");
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("На всяко тяло потопено в течност (газ), действа изтласкваща сила, равна по големина на теглото на течността (газа), изместена от тялото.");
+        }
+
+        private void button26_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Когато плътността на едно тяло е по-голяма от плътността на течността, в която е потопено, тялото се намира в равновесие във всяко място в течността, тоест то плава изцяло потопено в течността.");
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Когато плътността на едно тяло е по-малка от плътността на течността, в която е потопено, тялото плава, като само част от него е потопена.");
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Едно тяло плава, когато изтласкващата сила е равна на силата на тежестта на тялото.");
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хаотичното движение на градивните частици на веществата.");
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Сумата от кинетичната енергия У кинетично на топлинното движение и енергията на взаимодействие У потенциално на градивните частици се нарича вътрешна енергия.");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Когато плътността на едно тяло е по-голяма от плътността на течността, я която е потопено, тялото потъва.");
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Колкото по-ниско е разположен центърът на тежестта на едно тяло, толкова по-устойчиво е неговото равновесие.");
+        }
+
+        private void Formuli_Load(object sender, EventArgs e)
+        {
+            //WHAT THE FUCK IS UP WITH VISUAL STUDIOS' CODE. WHY CANT I USE BUTTON_CLICK INSTEAD OF BUTTON_CLICK_1 WHAT THE FUCK AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Тяло, спрямо което определяме положението и движението на другите тела.");
+        }
+
+        private void button23_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Бързината, с която се движи тяло в дадент момент от време.");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Физичната величина, която характеризира бързината на движещото тяло.");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Движение, при което се увеличава скоростта с постоянно ускорение.");
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Падане на различни тела във вакуум с еднакво ускорение, което не зависи от тяхната форма, маса, обем и плътност.");
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Движение, при което скоростта намалява с постоянно ускорение.");
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разстоянието от момента на натискане на спирачките до момента на спиране.");
+        }
+
+        private void button24_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Промяната на местоположението на едно тяло спрямо отправно тяло.");
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Свойството на телата да запазват състоянието си на покой или на праволинейно равномерно движение.");
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Количествената мярка за инертността на телата.");
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Всяко тяло запазва състоянието си на покой или на праволинейно равномерно движение, докато външно въздействие не го изведе от това състояние.");
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Равнодействащата сила F e равна на произведението на масата m и ускоринието a.");
+        }
+
+        private void button12_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Всяко действие има равно по сила и противоположно по посока противодействие.");
+        }
+
+        private void button13_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Физична величина, която определя действието на едно тяло върху друго тяло.");
+        }
+
+        private void button14_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Една сила може да извършва работа само когато тялото се премества.");
+        }
+
+        private void button15_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("A = Работа; F = Сила; S = Път; A = F*s > 0; A = -F*s < 0; A = 0.");
+        }
+
+        private void button27_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Налягането на течността, създадено от нейното тегло, се нарича хидростатично налягане.");
+        }
+
+        private void button30_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хидростатичното налягане (Ро) на дадена дълбочина h в течността зависи от дълбочината h и от нейната плътност.");
+        }
+
+        private void button28_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("F2/F1 = S2/S1");
+        }
+
+        private void button7_Click_2(object sender, EventArgs e)
+        {
+            MessageBox.Show("На всяко тяло потопено в течност (газ), действа изтласкваща сила, равна по големина на теглото на течността (газа), изместена от тялото.");
+        }
+
+        private void button17_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Когато плътността на едно тяло е по-голяма от плътността на течността, я която е потопено, тялото потъва.");
+        }
+
+        private void button26_Click_2(object sender, EventArgs e)
+        {
+            MessageBox.Show("Когато плътността на едно тяло е по-голяма от плътността на течността, в която е потопено, тялото се намира в равновесие във всяко място в течността, тоест то плава изцяло потопено в течността.");
+        }
+
+        private void button31_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Когато плътността на едно тяло е по-малка от плътността на течността, в която е потопено, тялото плава, като само част от него е потопена.");
+        }
+
+        private void button32_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Едно тяло плава, когато изтласкващата сила е равна на силата на тежестта на тялото.");
+        }
+
+        private void button33_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хаотичното движение на градивните частици на веществата.");
+        }
+
+        private void button34_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Сумата от кинетичната енергия У кинетично на топлинното движение и енергията на взаимодействие У потенциално на градивните частици се нарича вътрешна енергия.");
+        }
+
+        private void button35_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Колкото по-ниско е разположен центърът на тежестта на едно тяло, толкова по-устойчиво е неговото равновесие.");
+        }
+    }
+}
