5df0ddbd577e82fa3d5af67b1746e737ca90deb6
pAx24 <84711602+pAx24@users.noreply.github.com>
2022-11-10 20:51:27 +0200

    Knock Knock. New version you fock
    version 1.0.0.2

---
+++ 9th_grade_BETA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PC9G
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (!File.Exists("C:/PhysCalcTheme.txt"))
+            {
+                using (StreamWriter sw = File.CreateText("C:/PhysCalcTheme.txt"))
+                {
+                    sw.Write("dark");
+                }
+
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
