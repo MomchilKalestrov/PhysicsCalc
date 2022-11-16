d9e825536b16cff211d3bb8ef82b2a8ec15d1712
pAx24 <84711602+pAx24@users.noreply.github.com>
2022-11-16 19:41:52 +0200

    fixed crash when changing theme

---
+++ UC_definitions.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace PC9G
+{
+    partial class UC_definitions
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titlePanel1 = new System.Windows.Forms.Panel();
+            this.b13 = new System.Windows.Forms.Button();
+            this.titlePanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b1.FlatAppearance.BorderSize = 0;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.ForeColor = System.Drawing.Color.White;
+            this.b1.Location = new System.Drawing.Point(3, 34);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(168, 25);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "Електричен ток";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b3.FlatAppearance.BorderSize = 0;
+            this.b3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b3.ForeColor = System.Drawing.Color.White;
+            this.b3.Location = new System.Drawing.Point(3, 127);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(168, 25);
+            this.b3.TabIndex = 1;
+            this.b3.Text = "Големина на ел. ток\r\n";
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b4.FlatAppearance.BorderSize = 0;
+            this.b4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b4.ForeColor = System.Drawing.Color.White;
+            this.b4.Location = new System.Drawing.Point(3, 158);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(168, 25);
+            this.b4.TabIndex = 2;
+            this.b4.Text = "Закон на Ом";
+            this.b4.UseVisualStyleBackColor = false;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b5.FlatAppearance.BorderSize = 0;
+            this.b5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b5.ForeColor = System.Drawing.Color.White;
+            this.b5.Location = new System.Drawing.Point(3, 189);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(168, 25);
+            this.b5.TabIndex = 3;
+            this.b5.Text = "Закон за съпротивлението";
+            this.b5.UseVisualStyleBackColor = false;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b6.FlatAppearance.BorderSize = 0;
+            this.b6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b6.ForeColor = System.Drawing.Color.White;
+            this.b6.Location = new System.Drawing.Point(3, 220);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(168, 25);
+            this.b6.TabIndex = 4;
+            this.b6.Text = "Напрежение в-у резистор";
+            this.b6.UseVisualStyleBackColor = false;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b7.FlatAppearance.BorderSize = 0;
+            this.b7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b7.ForeColor = System.Drawing.Color.White;
+            this.b7.Location = new System.Drawing.Point(3, 251);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(168, 25);
+            this.b7.TabIndex = 5;
+            this.b7.Text = "Еквивалентно съпротивление";
+            this.b7.UseVisualStyleBackColor = false;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b8.FlatAppearance.BorderSize = 0;
+            this.b8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b8.ForeColor = System.Drawing.Color.White;
+            this.b8.Location = new System.Drawing.Point(3, 282);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(168, 25);
+            this.b8.TabIndex = 6;
+            this.b8.Text = "Последователно свързване";
+            this.b8.UseVisualStyleBackColor = false;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b9.FlatAppearance.BorderSize = 0;
+            this.b9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b9.ForeColor = System.Drawing.Color.White;
+            this.b9.Location = new System.Drawing.Point(3, 313);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(168, 25);
+            this.b9.TabIndex = 7;
+            this.b9.Text = "Успоредно свързване";
+            this.b9.UseVisualStyleBackColor = false;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(97)))), ((int)(((byte)(158)))));
+            this.b2.FlatAppearance.BorderSize = 0;
+            this.b2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b2.ForeColor = System.Drawing.Color.White;
+            this.b2.Location = new System.Drawing.Point(3, 65);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(168, 56);
+            this.b2.TabIndex = 8;
+            this.b2.Text = "Условия за протичане на ел. ток";
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b10
+            // 
+            this.b10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(85)))), ((int)(((byte)(0)))));
+            this.b10.FlatAppearance.BorderSize = 0;
+            this.b10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b10.ForeColor = System.Drawing.Color.White;
+            this.b10.Location = new System.Drawing.Point(3, 344);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(168, 25);
+            this.b10.TabIndex = 9;
+            this.b10.Text = "Работата на тока";
+            this.b10.UseVisualStyleBackColor = false;
+            this.b10.Click += new System.EventHandler(this.b10_Click);
+            // 
+            // b11
+            // 
+            this.b11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(85)))), ((int)(((byte)(0)))));
+            this.b11.FlatAppearance.BorderSize = 0;
+            this.b11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b11.ForeColor = System.Drawing.Color.White;
+            this.b11.Location = new System.Drawing.Point(3, 375);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(168, 25);
+            this.b11.TabIndex = 14;
+            this.b11.Text = " Мощност";
+            this.b11.UseVisualStyleBackColor = false;
+            this.b11.Click += new System.EventHandler(this.b11_Click);
+            // 
+            // b12
+            // 
+            this.b12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(85)))), ((int)(((byte)(0)))));
+            this.b12.FlatAppearance.BorderSize = 0;
+            this.b12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b12.ForeColor = System.Drawing.Color.White;
+            this.b12.Location = new System.Drawing.Point(3, 406);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(168, 25);
+            this.b12.TabIndex = 13;
+            this.b12.Text = " Мощността на ел. ток";
+            this.b12.UseVisualStyleBackColor = false;
+            this.b12.Click += new System.EventHandler(this.b12_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(18, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Електричен ток";
+            // 
+            // titlePanel1
+            // 
+            this.titlePanel1.Controls.Add(this.label1);
+            this.titlePanel1.Location = new System.Drawing.Point(3, 3);
+            this.titlePanel1.Name = "titlePanel1";
+            this.titlePanel1.Size = new System.Drawing.Size(168, 25);
+            this.titlePanel1.TabIndex = 12;
+            // 
+            // b13
+            // 
+            this.b13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(85)))), ((int)(((byte)(0)))));
+            this.b13.FlatAppearance.BorderSize = 0;
+            this.b13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b13.ForeColor = System.Drawing.Color.White;
+            this.b13.Location = new System.Drawing.Point(3, 437);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(168, 25);
+            this.b13.TabIndex = 15;
+            this.b13.Text = "Големина на съпротивления";
+            this.b13.UseVisualStyleBackColor = false;
+            this.b13.Click += new System.EventHandler(this.b13_Click);
+            // 
+            // UC_definitions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(50)))), ((int)(((byte)(56)))));
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.titlePanel1);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b1);
+            this.Name = "UC_definitions";
+            this.Size = new System.Drawing.Size(945, 500);
+            this.Load += new System.EventHandler(this.UC_definitions_Load);
+            this.titlePanel1.ResumeLayout(false);
+            this.titlePanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel titlePanel1;
+        private System.Windows.Forms.Button b13;
+    }
+}
